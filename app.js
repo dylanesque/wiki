70e8805594c395c936dbb549c3014426292026b3
Michael Caveney <dylanesque27@gmail.com>
2017-09-23 20:06:10 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-function wikiSearch() {
-	const searchTerm = document.getElementById('search').value;
-	const url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
-	console.log(url);
-}
+$(document).ready(function(){
+	$('#wikiSearch').click(function(){
+		var searchParams = $('#search').val();
+		var searchUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchParams + "&format=json&callback=?";	
+
+		$.ajax({
+			type: "GET", 
+			url: searchUrl, 
+			async: false, 
+			dataType: "json", 
+			success: function(data){
+				for(var i = 0; i < data[1].length; i++) {
+					$('#searchResults').prepend("<li><a href=" + data[3][0] + ">" 
+					+ data[1][0] + "</a><p>" + data[2][0]  + "</p></li>");
+				}
+			
+			}, 
+			error: function(error){
+				alert("Error!");
+			}
+		
+		});
+	})
+});
+	
